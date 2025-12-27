441489eec46513f90ef7c3e585c9ff9520e26617
Madhesh-B <madhesh5463@gmail.com>
2025-12-27 16:09:43 +0530

    install required dependencies and update environment variables

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const userRoutes = require('./contollers/User');
+const userRoutes = require('./controllers/User');
 const dotenv = require('dotenv');
 const app = express();
 dotenv.config();
